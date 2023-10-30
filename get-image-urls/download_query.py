b943860c03e22e94fba400ad70316b4afe27a916
Kalimar Maia <kmaia@pluribusdigital.com>
2023-10-30 16:39:52 -0400

    Modifying file on GitHub

--- get-image-urls/download_query.py
+++ get-image-urls/download_query.py
@@ -14,3 +14,12 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 job.commit()
+
+# add parent directory to sys path to import relative modules
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir = os.path.dirname(parentdir)
+sys.path.insert(0,parentdir)
+
+from lib.io_utils import *
+from lib.processing_utils import *
